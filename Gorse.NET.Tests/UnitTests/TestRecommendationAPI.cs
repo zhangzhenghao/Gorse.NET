2d57d10b482bedd8623dafcb1e627e199cbc4c53
zhenghaoz <zhenghaoz@qq.com>
2026-03-31 21:19:56 +0800

    fix: update tests to expect UserScore objects instead of strings

--- Gorse.NET.Tests/UnitTests/TestRecommendationAPI.cs
+++ Gorse.NET.Tests/UnitTests/TestRecommendationAPI.cs
@@ -10,9 +10,9 @@ public partial class Tests
         client.InsertUser(new Models.User { UserId = "3000" });
         var recommendations = client.GetRecommend("3000");
         Assert.That(recommendations, Is.Not.Null);
-        Assert.That(recommendations[0], Is.EqualTo("315"));
-        Assert.That(recommendations[1], Is.EqualTo("1432"));
-        Assert.That(recommendations[2], Is.EqualTo("918"));
+        Assert.That(recommendations[0].Id, Is.EqualTo("315"));
+        Assert.That(recommendations[1].Id, Is.EqualTo("1432"));
+        Assert.That(recommendations[2].Id, Is.EqualTo("918"));
     }
 
     [Test]
@@ -21,8 +21,8 @@ public partial class Tests
         await client.InsertUserAsync(new User { UserId = "4000" });
         var recommendations = await client.GetRecommendAsync("4000");
         Assert.That(recommendations, Is.Not.Null);
-        Assert.That(recommendations[0], Is.EqualTo("315"));
-        Assert.That(recommendations[1], Is.EqualTo("1432"));
-        Assert.That(recommendations[2], Is.EqualTo("918"));
+        Assert.That(recommendations[0].Id, Is.EqualTo("315"));
+        Assert.That(recommendations[1].Id, Is.EqualTo("1432"));
+        Assert.That(recommendations[2].Id, Is.EqualTo("918"));
     }
 }
